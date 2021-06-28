7217e81d0a8720efdb67021a3108eebd67530164
igor-bonny-tua <igor-bonny.tua@allianz.com>
2021-06-29 00:04:37 +0700

    feat(shortener): Change request and response payload

--- src/main/java/com/example/urlshort/controller/ShortenerController.java
+++ src/main/java/com/example/urlshort/controller/ShortenerController.java
@@ -1,13 +1,18 @@
 package com.example.urlshort.controller;
 
+import com.example.urlshort.dto.UrlDTO;
 import com.example.urlshort.entity.UrlStore;
 import com.example.urlshort.exception.InvalidURLException;
+import com.example.urlshort.mapper.UrlDTOMapper;
 import com.example.urlshort.service.UrlShortenerService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
 
 @RestController
 @Slf4j
@@ -18,17 +23,17 @@ public class ShortenerController {
     UrlShortenerService urlShortenerService;
 
     @PostMapping
-    public ResponseEntity create(@RequestBody final String url) {
+    public ResponseEntity create(@RequestBody final UrlDTO urlDTO) {
         // Validating the URL
         UrlValidator urlValidator = new UrlValidator(new String[]{"http", "https"});
-        if (!urlValidator.isValid(url)) {
+        if (!urlValidator.isValid(urlDTO.getUrl())) {
             throw new InvalidURLException("Invalid URL");
         }
 
         // Process the URL on valid
-        UrlStore urlStore = urlShortenerService.makeShort(url);
+        UrlStore urlStore = urlShortenerService.makeShort(urlDTO.getUrl());
 
-        return ResponseEntity.ok(urlStore.getId());
+        return ResponseEntity.ok(UrlDTOMapper.urlStoreToUrlDTO(urlStore));
     }
 
     @GetMapping("/{id}")
@@ -39,7 +44,9 @@ public class ShortenerController {
         // increment the counter with the hit
         urlStore = urlShortenerService.increaseHit(urlStore);
 
-        return ResponseEntity.ok(urlStore.getUrl());
+        return ResponseEntity.ok(UrlDTOMapper.urlStoreToUrlDTO(urlStore));
+
+//        return ResponseEntity.status(HttpStatus.FOUND).location(URI.create(urlStore.getUrl())).build();
     }
 
 }
